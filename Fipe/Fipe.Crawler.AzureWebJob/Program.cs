1c55cf780e712925658a41782ce0a145be02806e
Emerson Soares <on.emersonsoares@gmail.com>
2014-06-08 23:27:27 -0300

    Cleanup

---
+++ Fipe/Fipe.Crawler.AzureWebJob/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Jobs;
+
+namespace Fipe.Crawler.AzureWebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new JobHost();
+            host.RunAndBlock();
+        }
+    }
+}
